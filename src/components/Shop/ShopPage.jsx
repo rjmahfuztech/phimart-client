7d04fc1fea946b0de1a4282be45b445260c6ef0d
rjmahfuztech <rjmahfuztech@gmail.com>
2025-04-14 21:19:31 +0600

    used useFetchProducts custom hook to fetch data

--- src/components/Shop/ShopPage.jsx
+++ src/components/Shop/ShopPage.jsx
@@ -1,53 +1,23 @@
-import { useEffect, useState } from "react";
-import apiClient from "../../services/api-client";
+import { useState } from "react";
 import ProductList from "./ProductList";
 import Pagination from "./Pagination";
+import useFetchProducts from "../../hooks/useFetchProducts";
 
 const ShopPage = () => {
-  const [products, setProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [totalPages, setTotalPages] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
+  const { isLoading, products, totalPages } = useFetchProducts(currentPage);
 
-  //   useEffect(() => {
-  //     setIsLoading(true);
-  //       apiClient
-  //         .get(`/products/?page=${currentPage}`)
-  //         .then((res) => {
-  //           setProducts(res.data.results);
-  //           setTotalPages(Math.ceil(res.data.count / res.data.results.length));
-  //         })
-  //         .catch((err) => console.log(err))
-  //         .finally(() => setIsLoading(false));
-  //   }, [currentPage]);
-
-  const fetchProducts = async () => {
-    setIsLoading(true);
-    try {
-      const response = await apiClient.get(`/products/?page=${currentPage}`);
-      const data = await response.data;
-
-      setProducts(data.results);
-      setTotalPages(Math.ceil(data.count / data.results.length));
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchProducts();
-  }, [currentPage]);
+  // if loading
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen py-10">
+        <span className="loading loading-spinner loading-xl text-secondary"></span>
+      </div>
+    );
+  }
 
   return (
     <div className="max-w-[1400px] mx-auto px-4">
-      {/* Loading  */}
-      {isLoading && (
-        <div className="flex justify-center items-center h-screen py-10">
-          <span className="loading loading-spinner loading-xl text-secondary"></span>
-        </div>
-      )}
       <ProductList products={products} />
       <Pagination
         totalPages={totalPages}
