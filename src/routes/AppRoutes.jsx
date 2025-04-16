32d65adce2bf6803e2700def6d4575d9b2e7ae38
rjmahfuztech <rjmahfuztech@gmail.com>
2025-04-16 20:09:10 +0600

    register route added

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,6 +5,7 @@ import About from "../pages/About";
 import MainLayout from "../layouts/MainLayout";
 import Shop from "../pages/Shop";
 import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const AppRoutes = () => {
   return (
@@ -14,6 +15,7 @@ const AppRoutes = () => {
         <Route path="about" element={<About />} />
         <Route path="shop" element={<Shop />} />
         <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Route>
     </Routes>
   );
