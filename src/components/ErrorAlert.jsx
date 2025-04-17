1608de7b6901fb64b7e33d361850f71403c2bde7
rjmahfuztech <rjmahfuztech@gmail.com>
2025-04-17 10:02:13 +0600

    error text updated

--- src/components/ErrorAlert.jsx
+++ src/components/ErrorAlert.jsx
@@ -16,7 +16,7 @@ const ErrorAlert = ({ error }) => {
           d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
         />
       </svg>
-      <span className="text-lg font-semibold">Error!! {error}</span>
+      <span className="text-lg font-semibold">{error}</span>
     </div>
   );
 };
