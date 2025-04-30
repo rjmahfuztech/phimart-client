98225e1b80cf24490432a8db11ad6f8e0b379034
rjmahfuztech <rjmahfuztech@gmail.com>
2025-04-30 10:58:45 +0600

    default error message updated

--- src/components/handleApiError.js
+++ src/components/handleApiError.js
@@ -2,18 +2,20 @@ import Swal from "sweetalert2";
 // Handle API Error
 const handleApiError = (
   error,
-  defaultMessage = "Something Went Wrong, Try again!"
+  defaultMessage = "Something Went Wrong, try again!"
 ) => {
   let ifError = "";
   if (error.response && error.response.data) {
     const errorMessage = Object.values(error.response.data).flat().join("\n");
     ifError = errorMessage;
-  } else ifError = defaultMessage;
+  } else {
+    error ? (ifError = error) : (ifError = defaultMessage);
+  }
   if (ifError) {
     Swal.fire({
       icon: "error",
       title: "Oops...",
-      text: `${ifError} try again!`,
+      text: `${ifError}`,
     });
   }
 };
